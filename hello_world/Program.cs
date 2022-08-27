437cfd51c80971ce69f4ebb332ad1ae050ee8537
Dawid Eret <94192705+dawideret@users.noreply.github.com>
2022-08-27 15:48:35 +0100

    hello_world + variables

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public enum StudentIDs
+{
+    Chris = 15213,
+    John = 21214,
+    Bartley = 35424,
+    Mabel = 46316,
+    Courtney = 54216,
+}
 
 public class hello_world
 {
     public static void Main()
     {
-        System.Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World");
+        Console.WriteLine(Console.ReadLine());
+
+        int a = 1;
+        float b = 2f;
+        bool c = true;
+        string d = "Hello";
+        char e = 'e';
+        double f = 1.75;
+
+        var g = 1;
+        var h = "string";
+        var i = a + g; // 'g' and 'i' will be integers, while 'h' will be a string
+
+        StudentIDs searchedStudent = StudentIDs.Mabel;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(e);
+        Console.WriteLine(f);
+        Console.WriteLine(g);
+        Console.WriteLine(h);
+        Console.WriteLine(i);
     }
 }
