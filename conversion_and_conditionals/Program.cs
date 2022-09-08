6615176b8cc9b92277096f47ef6edd97822604f1
Dawid Eret <94192705+dawideret@users.noreply.github.com>
2022-09-08 22:56:10 +0100

    Conversions

---
+++ conversion_and_conditionals/Program.cs
@@ -0,0 +1,15 @@
+﻿public class conv_n_cond
+{
+    public static void Main()
+    {
+        // Conversion
+        int x = 100;
+        float y = 200f;
+        double z = 300;
+
+        string combined = Convert.ToString(x) + Convert.ToString(y) + Convert.ToString(z);
+        Console.WriteLine(combined);
+        float a = float.Parse(combined) / 123456.7898765321f;
+        Console.WriteLine(a);
+    }
+}
