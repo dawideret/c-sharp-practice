7510bc0325d4c08587d7bf5b0be1445ec1d9d84a
Dawid Eret <94192705+dawideret@users.noreply.github.com>
2022-09-09 22:06:41 +0100

    Arrays

---
+++ arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿class Arrays
+{
+    public static void Main()
+    {
+        // Arrays
+        int[] george_grades = { 5, 2, 6, 9, 9, 4, 6 };
+        char[] bobs_grades = { 'A', 'A', 'F', 'B' };
+
+        for (int i = 0; i < george_grades.Length; i++)
+        {
+            Console.Out.WriteLine(george_grades[i]);
+        }
+
+        // Multidimensional Arrays
+        int[,] two_dimensions = new int[10,10];
+
+        for (int x = 0; x < two_dimensions.Length; x++)
+        {
+            for (int y = 0; y < two_dimensions.GetLength(x); y++)
+            {
+
+            }
+        }
+    }
+}
