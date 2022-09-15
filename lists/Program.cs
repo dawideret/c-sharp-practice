5dc407865947170c887b9b0d1feb08f71113b7fe
Dawid Eret <94192705+dawideret@users.noreply.github.com>
2022-09-15 21:34:46 +0100

    lists

---
+++ lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class hello_world
+{
+    public static void Main()
+    {
+        List<string> words = new();
+
+        words.Add("Apple");
+        words.Add("Watch");
+        words.Add("Is");
+        words.Add("Too");
+        words.Add("Expensive");
+
+        string[] words_array = new string[] { "Change", "My", "Mind" };
+
+        words.AddRange(words_array);
+        words.Remove("Too");
+        words.RemoveAt(1);
+
+        for (int i = 0; i < words.Count; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+    }
+}
